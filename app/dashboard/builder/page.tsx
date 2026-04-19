14dcf72a994990c85466b6cd5a7a1ed1fe125865
Is5555 <ashiqashik621@gmail.com>
2026-04-20 01:48:06 +0530

    Create builder dashboard

---
+++ app/dashboard/builder/page.tsx
@@ -0,0 +1,8 @@
+// Builder Dashboard
+import React from 'react';
+
+const BuilderDashboard = () => {
+  return <div>Builder Dashboard with stats, verification banner, subscription banner, and more.</div>;
+};
+
+export default BuilderDashboard;
